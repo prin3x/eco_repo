fa090c9e56dffc616695b20746b792109c148e73
prin3x <sarut1994@gmail.com>
2020-07-31 12:31:37 +0700

    cartIcon and cartDropdonw

--- src/redux/root-reducer.js
+++ src/redux/root-reducer.js
@@ -1,6 +1,8 @@
 import { combineReducers } from "redux";
 import userReducer from "./user/user.reducer.js";
+import cartReducer from "./cart/cart.reducer";
 
 export default combineReducers({
   user: userReducer,
+  cart: cartReducer,
 });
